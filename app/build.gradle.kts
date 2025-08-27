6262e5dcf4cb3bacea02cb90ccfb60653d592718
Vusal Islamzada <vusal.islamzada@abc-telecom.az>
2025-08-27 15:46:06 +0400

    Http Logger

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.islamzadaLibs"
             artifactId = "http-logger-sdk-android"
-            version = "1.0.1"
+            version = "1.0.2"
 
             afterEvaluate {
                 from(components["release"])
